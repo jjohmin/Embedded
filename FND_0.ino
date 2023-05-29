bd7cd6a2d04b9d5b3e22877e0e50541ae10d9afb
d <jhmin7624@gmail.com>
2023-05-30 07:32:21 +0900

    FND_0

---
+++ FND_0.ino
@@ -0,0 +1,21 @@
+int seg[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+int i;
+
+void setup() {
+  for (i = 0; i < 8; i++) {
+    pinMode(seg[i], OUTPUT);
+  }
+}
+
+void loop() {
+  digitalWrite(seg[0], LOW);
+  digitalWrite(seg[1], LOW);
+  digitalWrite(seg[2], LOW);
+  digitalWrite(seg[3], LOW);
+  digitalWrite(seg[4], LOW);
+  digitalWrite(seg[5], LOW);
+  digitalWrite(seg[6], HIGH);
+  digitalWrite(seg[7], LOW);
+  digitalWrite(seg[8], HIGH);
+  delay(1000);
+}
